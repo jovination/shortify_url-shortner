a91ae46a4435f95cfa12033cb124002f05c78f78
Jovin Shija <jovination791@gmail.com>
2023-12-24 01:19:41 +0300

    writing js for link and Qrcode buttons

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,37 @@ document.addEventListener('DOMContentLoaded', function() {
     fun__btn1.onclick = () => {
         fun__btn1.classList.add('active');
         fun__btn2.classList.remove('active');
+        showLinkIcon1();
     };
 
     fun__btn2.onclick = () => {
         fun__btn2.classList.add('active');
         fun__btn1.classList.remove('active');
+        showQrIcon1();
     }
+
+    showLinkIcon1();
 });
+
+
+function showLinkIcon1() {
+    let fun__link1 = document.querySelector('.link1');
+    let fun__link2 = document.querySelector('.link2');
+    let fun__qr1 = document.querySelector('.qr1');
+    let fun__qr2 = document.querySelector('.qr2');
+    fun__link1.style.display = 'block';
+    fun__link2.style.display = 'none';
+    fun__qr1.style.display = 'block';
+    fun__qr2.style.display = 'none';
+}
+
+function showQrIcon1() {
+    let fun__qr1 = document.querySelector('.qr1');
+    let fun__qr2 = document.querySelector('.qr2');
+    let fun__link1 = document.querySelector('.link1');
+    let fun__link2 = document.querySelector('.link2');
+    fun__qr1.style.display = 'none';
+    fun__qr2.style.display = 'block';
+    fun__link1.style.display = 'none';
+    fun__link2.style.display = 'block';
+}
