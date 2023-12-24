c65e2d44991eca683892b966ff83d0e430784393
Jovin Shija <jovination791@gmail.com>
2023-12-24 14:21:17 +0300

    adding some js for affecting the DOM

--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
     let fun__btn1 = document.querySelector('.btn1');
     let fun__btn2 = document.querySelector('.btn2');
+    let f__header = document.querySelector('.f-header');
+    let fun__link1 = document.querySelector('.link1');
+    let fun__link2 = document.querySelector('.link2');
+    let fun__qr1 = document.querySelector('.qr1');
+    let fun__qr2 = document.querySelector('.qr2');
+    let qr__txt = "generate QR Code";
+    let original__txt = f__header.innerHTML; // store the original text
 
     fun__btn1.onclick = () => {
         fun__btn1.classList.add('active');
         fun__btn2.classList.remove('active');
         showLinkIcon1();
+        if (fun__btn1.classList.contains('active')) {
+            f__header.innerHTML = original__txt; // set to original text
+        }
     };
 
     fun__btn2.onclick = () => {
         fun__btn2.classList.add('active');
         fun__btn1.classList.remove('active');
         showQrIcon1();
+        generateQrCode();
     };
+
+    function generateQrCode() {
+        if (fun__btn2.classList.contains('active')) {
+            f__header.innerHTML = qr__txt;
+        }
+    }
+
+    function showLinkIcon1() {
+        fun__link1.style.display = 'block';
+        fun__link2.style.display = 'none';
+        fun__qr1.style.display = 'block';
+        fun__qr2.style.display = 'none';
+    }
+
+    function showQrIcon1() {
+        fun__qr1.style.display = 'none';
+        fun__qr2.style.display = 'block';
+        fun__link1.style.display = 'none';
+        fun__link2.style.display = 'block';
+    }
 
     showLinkIcon1();
 });
-
-
-function showLinkIcon1() {
-    let fun__link1 = document.querySelector('.link1');
-    let fun__link2 = document.querySelector('.link2');
-    let fun__qr1 = document.querySelector('.qr1');
-    let fun__qr2 = document.querySelector('.qr2');
-    fun__link1.style.display = 'block';
-    fun__link2.style.display = 'none';
-    fun__qr1.style.display = 'block';
-    fun__qr2.style.display = 'none';
-}
-
-function showQrIcon1() {
-    let fun__qr1 = document.querySelector('.qr1');
-    let fun__qr2 = document.querySelector('.qr2');
-    let fun__link1 = document.querySelector('.link1');
-    let fun__link2 = document.querySelector('.link2');
-    fun__qr1.style.display = 'none';
-    fun__qr2.style.display = 'block';
-    fun__link1.style.display = 'none';
-    fun__link2.style.display = 'block';
-}
