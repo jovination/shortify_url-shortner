48c2adca74ca138cbe7e04ea785e3f089cb7b45e
jovination <jovination791@gmail.com>
2024-10-05 04:29:00 +0300

    working on responsiveness

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
